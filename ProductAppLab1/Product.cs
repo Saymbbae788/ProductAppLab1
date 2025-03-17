61d0ae8c938993d91275053eb5b41c3357209ac8
Федоренко <saymbbae@gmail.com>
2025-03-17 08:50:05 +0300

    Добавлен класс Product

---
+++ ProductAppLab1/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Product
+{
+    public string Name { get; set; }
+    public int Quantity { get; set; }
+    public float Price { get; set; }
+
+    public Product(string name, int quantity, float price)
+    {
+        Name = name;
+        Quantity = quantity;
+        Price = price;
+    }
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"Товар: {Name}, Количество: {Quantity}, Стоимость: {Price} руб.");
+    }
+}
